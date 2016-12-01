a2cbfc51a422fbde9b17957d382f9815d32b0c8f
Andrew <arellok@gmail.com>
2016-11-30 18:29:54 -0800

    added wall prefab and createRoom

--- Assets/Scripts/ManageTest.cs
+++ Assets/Scripts/ManageTest.cs
@@ -4,16 +4,18 @@ using Valve.VR;
 
 public class ManageTest : MonoBehaviour {
 	public GameObject floor;
+	public GameObject wall;
 	private GameObject theFloor;
 	private GameObject[] destroying;
 	private int posX, posZ = 0;
 	private int count = 0;
 	public float scaleX, scaleZ;
+	public Vector3 corner1, corner2, corner3, corner4;
 
 	void Start () {
 		var rect = new HmdQuad_t();
 		//Debug.Log(SteamVR_PlayArea.GetBounds());
-		createFloor();
+		createRoom();
 	}
 
 	void Update () {
@@ -22,7 +24,7 @@ public class ManageTest : MonoBehaviour {
 		}
 
 		if (Input.GetMouseButtonDown(0)){
-			createFloor();
+			createRoom();
 		}
 	}
 
@@ -30,8 +32,12 @@ public class ManageTest : MonoBehaviour {
 		destroyAllObjects();
 	}
 
-	public void createFloor(){
+	public void createRoom(){
 		Instantiate(floor, new Vector3(0, 0, 0), Quaternion.Euler(90, 0, 0));
+
+
+
+
 		/*
 		for (int z = 0; z < 10; z++){
 			for (int x = 0; x < 10; x++){
